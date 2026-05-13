44786bd1c81a22bfa53cfa8215ab72383aaaf64d
Skand Choudhary <164895611+SKAND9337@users.noreply.github.com>
2026-05-13 15:59:56 +0530

    theoretical noctalia-colors.lua

---
+++ hypr/noctalia/noctalia-colors.lua
@@ -0,0 +1,30 @@
+-- ~/.config/hypr/noctalia/noctalia-colors.lua
+
+hl.config({
+    general = {
+        col = {
+            active_border = "rgb(67d2e4)",
+            inactive_border = "rgb(0e252f)",
+        },
+    },
+
+    group = {
+        col = {
+            border_active = "rgb(5c86d6)",
+            border_inactive = "rgb(0e252f)",
+
+            border_locked_active = "rgb(fd4663)",
+            border_locked_inactive = "rgb(0e252f)",
+        },
+
+        groupbar = {
+            col = {
+                active = "rgb(5c86d6)",
+                inactive = "rgb(0e252f)",
+
+                locked_active = "rgb(fd4663)",
+                locked_inactive = "rgb(0e252f)",
+            },
+        },
+    },
+})
